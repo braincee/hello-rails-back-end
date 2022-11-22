988d13b1b46ab4bc5ce29e0041f7385f06a75a72
braincee <kwesi938@gmail.com>
2022-11-22 11:30:16 +0000

    setup greeting controller and route actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'api/v1/greeting', to: 'application#greeting'
+  
+  root 'api/v1/application#greeting'
 end
