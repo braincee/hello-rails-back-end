3822103bd9b4ed795784386fe73ad1c3db6e3a3e
braincee <kwesi938@gmail.com>
2022-11-22 11:37:39 +0000

    Add cross-origin resource sharing(CORS)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'api/v1/greeting', to: 'application#greeting'
-  
-  root 'api/v1/application#greeting'
+
+  root 'application#greeting'
 end
